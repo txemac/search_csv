a86cf7c3442c3786fae5ce2f6c64790c068901fd
Jose Bermudez <txema.bermudez@gmail.com>
2016-05-11 19:34:32 +0100

    setting file

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+
+__author__ = 'josebermudez'
+
+
+# Base directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Name of the file with catalogue
+# DATASET_FILE = 'search_dataset.csv'
+DATASET_FILE = 'search_dataset_test.csv'
