f527afd699741919b1c363b3b36b310abdaac1ad
Jose Bermudez <txema.bermudez@gmail.com>
2016-05-12 02:07:04 +0100

    setting add scores

--- settings.py
+++ settings.py
@@ -7,3 +7,10 @@ DATASET_TESTS_FILE = 'tests/search_dataset_test.csv'
 
 # Query file
 QUERIES_FILE = 'queries.txt'
+
+
+# Score
+POINTS_TOKEN = 10
+POINTS_PREFIX = 5
+POINTS_MUL_NAME = 2
+POINTS_MUL_BRAND = 1
